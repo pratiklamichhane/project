e80d7a81937ab5244b484f4b03f8f5cb9d3019e2
Pratik Lamichhane <110966247+pratiklamichhane@users.noreply.github.com>
2025-01-27 21:18:56 +0545

    Update Hero component description for clarity and engagement

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,7 +4,7 @@
           Hey I'm Pratik
         </h1>
         <p class="mb-16 text-gray-300">
-          I'm Pratik, a full-stack developer from Nepal, passionate about crafting efficient web applications with Laravel, Vue.js, React, and modern tools. As a Software Developer at F.A.C.T Pvt Ltd, I tackle innovative projects and complex challenges. Beyond coding, I love sharing knowledge through writing, empowering fellow developers, and exploring the endless possibilities of tech.
+          Hi, I’m Pratik! I’m a full-stack developer from Nepal with a passion for creating web applications, APIs, and hybrid mobile and desktop apps using tools like Laravel, Vue.js, React , PHP Native and ionic. I work on transforming ideas into functional, real-world solutions and enjoy taking on exciting challenges. Beyond coding, I love sharing my knowledge to support other developers and constantly exploring new opportunities in tech. Let’s collaborate and build something amazing together!
         </p>
       </section>
 </template>
